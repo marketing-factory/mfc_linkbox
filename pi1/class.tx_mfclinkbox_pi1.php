38d1e6531a495ab31c4afb60ca0e28bf3efa79d3
Simon Schmidt <sfs@marketing-factory.de>
2014-05-06 09:55:31 +0000

    delete require_once

--- pi1/class.tx_mfclinkbox_pi1.php
+++ pi1/class.tx_mfclinkbox_pi1.php
@@ -22,7 +22,6 @@
 *  This copyright notice MUST APPEAR in all copies of the script!
 ***************************************************************/
 
-require_once(PATH_tslib . 'class.tslib_pibase.php');
 
 /**
  * Plugin 'Linkbox' for the 'mfc_linkbox' extension.
